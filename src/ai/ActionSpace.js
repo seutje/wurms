f3b637a99e0ca3258c48dfb6fc6cf200cc811371
seutje <seutje@gmail.com>
2025-07-23 17:54:11 +0200

    Clean up.

--- src/ai/ActionSpace.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.WEAPON_CHOICES = void 0;
-exports.WEAPON_CHOICES = [
-    'bazooka',
-    'grenade',
-    'mortar',
-    'nuke',
-];
