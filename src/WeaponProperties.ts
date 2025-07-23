1106335add0191c59dbf272ca9372723559debef
seutje <seutje@gmail.com>
2025-07-23 23:50:51 +0200

    Tweak weapon damage.

--- src/WeaponProperties.ts
+++ src/WeaponProperties.ts
@@ -1,6 +1,6 @@
 export const weaponProperties: { [key: string]: { radius: number, damage: number, explosionRadius: number } } = {
-  bazooka: { radius: 5, damage: 30, explosionRadius: 20 },
-  grenade: { radius: 5, damage: 40, explosionRadius: 20  },
-  mortar: { radius: 5, damage: 35, explosionRadius: 20  },
-  nuke: { radius: 10, damage: 100, explosionRadius: 50  },
+  bazooka: { radius: 5, damage: 10, explosionRadius: 20 },
+  grenade: { radius: 5, damage: 15, explosionRadius: 20  },
+  mortar: { radius: 5, damage: 10, explosionRadius: 20  },
+  nuke: { radius: 10, damage: 25, explosionRadius: 50  },
 };
