19c087867bc3bc565c79cb1cd82486cb27e460d0
Steve De Jonghe <seutje@gmail.com>
2025-07-24 09:24:57 +0200

    Fix: bind terrain helpers

--- src/Terrain.ts
+++ src/Terrain.ts
@@ -79,21 +79,21 @@ export class Terrain extends GameObject {
     return baseHeight + total;
   };
 
-  private generateNoiseParameters() {
+  private generateNoiseParameters = () => {
     const octaves = 4;
     for (let i = 0; i < octaves; i++) {
       this.phases[i] = this.random() * Math.PI * 2;
       this.amplitudes[i] =
         (this.height / 4) * (0.5 ** i) * (0.5 + this.random() * 0.5);
     }
-  }
+  };
 
-  private mulberry32(a: number) {
+  private mulberry32 = (a: number) => {
     return () => {
       let t = (a += 0x6d2b79f5);
       t = Math.imul(t ^ (t >>> 15), t | 1);
       t ^= t + Math.imul(t ^ (t >>> 7), t | 61);
       return ((t ^ (t >>> 14)) >>> 0) / 4294967296;
     };
-  }
+  };
 }
